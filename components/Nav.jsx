34ecf98c714e323bac72878680fb296e64f66685
julio <julior0518@gmail.com>
2022-04-12 11:32:02 -0400

    created react app & components, pages, assets folder

---
+++ components/Nav.jsx
@@ -0,0 +1,11 @@
+
+function Nav () {
+
+    return (
+        <div className="Nav">
+            Nav
+        </div>
+    )
+}
+
+export default Nav;
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Route, Switch} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+        hola
+      <Switch>
+        {/* <Route exact path="/" render={} />
+        <Route exact path="/profile" render={()=> <Profile/>} /> */}
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
